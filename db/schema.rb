b6fa00a24e95c9e1d4125790952dc45890bd8cb7
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-20 11:50:16 -0700

    Added migration to change product price to integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180420175219) do
+ActiveRecord::Schema.define(version: 20180420184759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,7 +69,7 @@ ActiveRecord::Schema.define(version: 20180420175219) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.float "price"
+    t.integer "price"
     t.string "description"
     t.string "image"
     t.integer "stock"
