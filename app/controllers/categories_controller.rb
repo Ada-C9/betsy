b9af8ee367cdd69a84032f2069ab21f8f16a3ec0
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-20 00:30:09 -0700

    Very rough work design on category, product, and user controllers done. All still completely untested.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,31 @@
 class CategoriesController < ApplicationController
+
   def index
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created a category for #{@category.name}!"
+      redirect_to: user_path(@user)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create this category."
+      flash[:messages] = @category.errors.messages
+      render :new, status: :bad_request
+    end
   end
+
+  private
+
+  def category_params
+  params.require(:category).permit(:name)
+end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+
   def index
+    @users = @user.all
   end
 
   def show
+    @user = User.find_by(:id params[:id])
   end
 end
