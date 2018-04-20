72a4a9c9463a2e05243e51d5fac44bffaa9169ec
Wenjie <traxiaojie@gmail.com>
2018-04-19 21:06:59 -0700

    Added cartitems controller

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -1,2 +1,12 @@
 class CartitemsController < ApplicationController
+  def change_quantity
+
+  end
+
+  def destroy
+    @cartitem.destroy
+    flash[:status] = :success
+    flash[:result_text] = "Successfully removed item #{@cartitem.product.name}"
+    redirect_to
+  end
 end
