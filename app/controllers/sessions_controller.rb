226dc88dfaf513a35570f8e220db650fae3183f4
Wenjie <traxiaojie@gmail.com>
2018-04-19 17:17:59 -0700

    Added build from github method in Merchant model Added login and logout functions in merchants controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,32 @@
 class SessionsController < ApplicationController
+
+  def login
+    auth_hash = request.env["omniauth.auth"]
+
+    if auth_hash["uid"]
+      @merchant = Merchant.find_by(uid: auth_hash["uid"], provider: auth_hash["provider"])
+
+      if @merchant.nil?
+        @merchant = Merchant.build_from_github(auth_hash)
+        flash[:status] = :success
+        flash[:result_text] = "#{@merchant.username} successfully logged in as a new merchant"
+      else
+        flash[:status] = :success
+        flash[:result_text] = "#{@merchant.username} successfully logged in as existing merchant"
+      end
+      session[:merchant_id] = @merchant.id
+
+    else
+      flash[:error] = "Could not log in"
+    end
+
+    redirect_to root_path
+  end
+
+  def logout
+    session[:merchant_id] = nil
+    flash[:status] = :success
+    flash[:result_text] = "successfully logged out"
+    redirect_to root_path
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,6 +1,22 @@
 class Merchant < ApplicationRecord
   has_many :products
-  
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
+
+  def self.build_from_github(auth_hash)
+    merchant = Merchant.new(
+      username: auth_hash[:info][:name],
+      email: auth_hash[:info][:email],
+      uid: auth_hash[:uid],
+      provider: auth_hash[:provider]
+    )
+
+    if merchant.save
+      return merchant
+    else
+      raise ArgumentError.new
+    end
+  end
+
 end
