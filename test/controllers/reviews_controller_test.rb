4cbbc3f7ae12bfde3be8e2d53897d5ec237d71db
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2018-04-20 11:24:20 -0700

    Cleaned up some of the Person 2 Assigned Controllers

---
+++ test/controllers/reviews_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+describe ReviewsController do
+  describe 'index' do
+    it "works with multiple reviews" do
+      skip
+    end
+
+    it 'works with no reviews' do
+      skip
+    end
+  end
+
+  describe 'show' do
+    it 'finds an exsisting review' do
+      skip
+    end
+
+    it 'renders 404 not found for a fake id' do
+      skip
+    end
+  end
+end
