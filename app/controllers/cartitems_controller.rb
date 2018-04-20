123fb6e28a0ecf1a837aba6b46d30b06867b5e45
Wenjie <traxiaojie@gmail.com>
2018-04-19 21:24:34 -0700

    Created yaml file for products, cartitems and merchants

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -1,12 +1,12 @@
 class CartitemsController < ApplicationController
-  def change_quantity
-
+  def change_quantity(quantity)
+   
   end
 
   def destroy
     @cartitem.destroy
     flash[:status] = :success
     flash[:result_text] = "Successfully removed item #{@cartitem.product.name}"
-    redirect_to
+    redirect_to cartitem_path(@cartitem.cart)
   end
 end
