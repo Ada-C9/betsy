61f8a5c77d4eef17de4f36d3b490e3fb2e725c36
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-21 13:02:51 -0700

    Changes to the application and users controller to facilitate implementation of Oauth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
     render_404 unless @user
+    @user_products = @user.products
+    # Model method for getting the relevant orders is needed
   end
 
 end
