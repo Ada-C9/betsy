f29ee4dfde6763791a47f3ce67525a1dbc336cef
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-21 16:12:36 -0700

    Add methods and test for order_item

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,8 +2,8 @@ class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validate :check_product_is_active
-  validate :check_quantity
+  validate :check_product_is_active, if: :contains_product?
+  validate :check_quantity, if: :contains_product?
 
   def check_product_is_active
     if !self.product.is_active
@@ -12,8 +12,18 @@ class OrderItem < ApplicationRecord
   end
 
   def check_quantity
-    if self.quantity > self.product.stock
+    if self.quantity.to_i > self.product.stock
       errors.add(:stock, "only #{self.product.stock} items available")
     end
   end
+
+  def subtotal
+    ((quantity * product.price)/100.0).round(2)
+  end
+
+  private
+  def contains_product?
+    !self.product.nil?
+  end
+
 end
--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -2,8 +2,81 @@ require "test_helper"
 
 describe OrderItem do
   let(:order_item) { OrderItem.new }
+  let(:oi) { order_items(:order_item_1) }
 
-  # it "must be valid" do
-  #   value(order_item).must_be :valid?
-  # end
+
+  describe "relations" do
+    it "has an order" do
+      item = order_items(:order_item_1)
+      item.must_respond_to :order
+      item.order.must_be_kind_of Order
+      item.order.must_equal orders(:order_1)
+    end
+    it "has a product" do
+      item = order_items(:order_item_1)
+      item.must_respond_to :product
+      item.product.must_be_kind_of Product
+      item.product.must_equal products(:product_1)
+    end
+
+    it "changes in order_id and product_id reflects in order and product" do
+      item = order_items(:order_item_1)
+      item.order_id = orders(:order_2).id
+      item.order.must_equal orders(:order_2)
+
+      item.product_id = products(:product_2).id
+      item.product.must_equal products(:product_2)
+    end
+  end
+
+  describe "validations" do
+
+    it "has validation for empty parameters" do
+      order_item.valid?.must_equal false
+    end
+
+    it "has validation for quantity presence" do
+      oi.quantity = nil
+      oi.valid?.must_equal false
+      oi.errors.messages.must_include :quantity
+
+      oi.quantity = ""
+      oi.valid?.must_equal false
+      oi.errors.messages.must_include :quantity
+    end
+
+    it "has quantity as integer, greater than 0" do
+      ["one", -1, 0].each {|element|
+        order_item.quantity = element
+        order_item.valid?.must_equal false
+      }
+    end
+
+    it "checks product is active" do
+      oi.product.is_active = false
+      oi.valid?.must_equal false
+      oi.errors.messages.must_include :is_active
+    end
+
+    it "checks quantity is available" do
+      oi.product.stock = 0
+      oi.valid?.must_equal false
+      oi.errors.messages.must_include :stock
+
+      order_items(:order_item_6).product.stock = 2
+      order_items(:order_item_6).valid?.must_equal false
+      order_items(:order_item_6).errors.messages.must_include :stock
+    end
+
+    it "checks subtotal" do
+      oi.product.price = 299
+      oi.subtotal.must_equal 2.99
+      oi.product.price = 29
+      oi.subtotal.must_equal 0.29
+    end
+
+    it "must be valid" do
+      oi.valid?.must_equal true
+    end
+  end
 end
