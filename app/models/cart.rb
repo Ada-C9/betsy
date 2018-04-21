f063a16be7615623f1e4eac4dea4b020a769437d
Wenjie <traxiaojie@gmail.com>
2018-04-20 23:37:18 -0700

    Added funtion to calculate subtotal for the cart and Implemented test for the subtotal function

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,12 @@ class Cart < ApplicationRecord
   has_many :cartitems
   has_many :products, through: :cartitems
 
-  def total_cost
-
+  def subtotal
+    sub_total = 0
+    self.cartitems.each do |cartitem|
+      sub_total += cartitem.subtotal
+    end
+    # self.cartitems.inject {|total, cartitem.subtotal| total + subtotal}
+    return sub_total
   end
 end
--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -7,4 +7,11 @@ describe Cart do
     value(cart).must_be :valid?
   end
 
+  describe "subtotal" do
+    it "calculates subtotal for all items in the cart" do
+      cart1 = Cart.first
+      result = cart1.subtotal
+      result.must_equal 17.16
+    end
+  end
 end
