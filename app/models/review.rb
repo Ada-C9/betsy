1c47fbf93851d8b8c398675ce9e4ef645a596cca
SelamawitA <Selamawit.ainalem@gmail.com>
2018-04-22 15:47:43 -0700

    rating for a product now display first by most recently posted

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
   validates :rating, presence: true, numericality: { only_integer: true }
   validates_inclusion_of :rating, in: (1..5)
+
+
 end
