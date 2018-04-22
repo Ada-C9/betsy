ee67a009d330d754e448fbab7e47907a2a8389ec
Wenjie <traxiaojie@gmail.com>
2018-04-22 11:31:07 -0700

    Created merchants controller and index and show action

---
+++ app/controllers/merchants_controller.rb
@@ -0,0 +1,11 @@
+class MerchantsController < ApplicationController
+  def index
+    @merchants = Merchant.all
+  end
+
+  def show
+    @merchant = Merchant.find_by(id: params[:id])
+    head :not_found unless @merchant
+  end
+
+end
