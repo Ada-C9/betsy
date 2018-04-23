e537d37f4046168aae711320ba33191312db0ad7
Wenjie <traxiaojie@gmail.com>
2018-04-23 13:47:05 -0700

    Added business logic to show certain products in model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,12 @@
 class Category < ApplicationRecord
   has_and_belongs_to_many :products
 
-  validates :name, presence: true, uniqueness: true 
+  validates :name, presence: true, uniqueness: true
+
+  def show_four
+    if self.products.count > 4
+      return self.products.first(4)
+    end
+    return self.products
+  end
 end
