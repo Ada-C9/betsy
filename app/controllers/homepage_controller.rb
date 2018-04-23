3c16e28ddbf2454dc330877f9eb54d9a3cff099b
Zheng Cao <chori84@gmail.com>
2018-04-23 11:11:14 -0700

    Add popular list

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,5 +1,6 @@
 class HomepageController < ApplicationController
   def index
+    @popular_products = Product.top_sellers
     category_outfit = Category.find_by(name: "Outfit")
     @outfits = category_outfit.products
     category_toy = Category.find_by(name: "Toys")
