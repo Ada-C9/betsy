175ca2d72768dce46bb7ac723ba5d1601e67e5d4
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-23 15:40:48 -0700

    search method and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :products
   has_many :order_items, through: :products
+  has_many :orders, through: :order_items
 
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: true
