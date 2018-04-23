fa0997548aba3c499755855030d9727ed5989385
Wenjie <traxiaojie@gmail.com>
2018-04-23 10:33:33 -0700

    Fixed bug in cartitem controller to delpoy to heroku

--- app/controllers/cartitems_controller.rb
+++ app/controllers/cartitems_controller.rb
@@ -32,41 +32,42 @@ class CartitemsController < ApplicationController
 
         end
       end
-
-
-
-      def update
-        @cartitem.update_attributes(quantity: params[:cartitem][:quantity])
-
-        if @cartitem.product.available?(@cartitem.quantity)
-          if @cartitem.save
-            flash[:status] = :success
-            flash[:result_text] = "Successfully updated your #{@cartitem.product.name} quantity"
-          else
-            flash[:status] = :failure
-            flash[:result_text] = "Could not update your #{@cartitem.product.name} quantity"
-            flash[:messages] = @cartitem.errors.messages
-          end
-
-        else
-          flash[:status] = :failure
-          flash[:result_text] = "There is not enough #{@cartitem.product.name} in stock"
-        end
-        redirect_to cart_path(@cartitem.cart_id)
-      end
-
-      def destroy
-        @cartitem.destroy
-        flash[:status] = :success
-        flash[:result_text] = "Successfully removed item #{@cartitem.product.name}"
-        redirect_to cart_path(@cartitem.cart_id)
-      end
-
-      private
-      def find_cartitem
-        @cartitem = Cartitem.find_by(id: params[:id])
-
-        head :not_found unless @cartitem
-      end
-
     end
+  end
+
+
+  def update
+    @cartitem.update_attributes(quantity: params[:cartitem][:quantity])
+
+    if @cartitem.product.available?(@cartitem.quantity)
+      if @cartitem.save
+        flash[:status] = :success
+        flash[:result_text] = "Successfully updated your #{@cartitem.product.name} quantity"
+      else
+        flash[:status] = :failure
+        flash[:result_text] = "Could not update your #{@cartitem.product.name} quantity"
+        flash[:messages] = @cartitem.errors.messages
+      end
+
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "There is not enough #{@cartitem.product.name} in stock"
+    end
+    redirect_to cart_path(@cartitem.cart_id)
+  end
+
+  def destroy
+    @cartitem.destroy
+    flash[:status] = :success
+    flash[:result_text] = "Successfully removed item #{@cartitem.product.name}"
+    redirect_to cart_path(@cartitem.cart_id)
+  end
+
+  private
+  def find_cartitem
+    @cartitem = Cartitem.find_by(id: params[:id])
+
+    head :not_found unless @cartitem
+  end
+
+end
