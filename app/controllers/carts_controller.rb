f12a561d64967b0bb74c5860654f603dd4d80419
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-22 20:11:16 -0700

    added and update_create_cart action to cart controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,23 @@
 class CartsController < ApplicationController
   before_action :find_cart
+  def new
+    @cart = Cart.new
+  end
+
+  def update_create_cart
+    if session[:cart_id]
+      @cart = Cart.find_by(id: session[:cart_id])
+    else
+      @cart = Cart.new
+      session[:cart_id] = @cart.id
+    end
+  end
+
+
   def show
   end
+
+
 
   private
   def find_cart
