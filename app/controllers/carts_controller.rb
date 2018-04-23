b271e64ec54aeaa74015e61cb042a9820ea766fe
Caroline Nardi <caroline.m.nardi@gmail.com>
2018-04-22 23:48:53 -0700

    Made cart show page. Updated cart and cartitem model methods but have not created tests for them yet. Created view helper for displaying prices and totals in dollars

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,7 +1,23 @@
 class CartsController < ApplicationController
-  before_action :find_cart
-  def show
+  before_action :find_cart, only: [:show]
+
+  # may not need this eventually
+  def create
+    @cart = Cart.new
+
+    if @cart.save
+      flash[:status] = :success
+      flash[:result_text] = "Successfully created cart"
+      redirect_to carts_path(@cart)
+    else
+      flash[:status] = :failure
+      flash[:result_text] = "Could not create cart"
+      flash[:messages] = @cart.errors.messages
+      redirect_back(fallback_location: root_path)
+    end
   end
+
+  def show; end
 
   private
   def find_cart
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def display_dollars(cents)
+    dollars = cents / 100.0
+    return '%.2f' % dollars
+  end
 end
