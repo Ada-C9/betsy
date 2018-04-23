d683132adad51f435a7c731244eddddae5cba0c0
Zheng Cao <chori84@gmail.com>
2018-04-22 19:14:21 -0700

    Link Update & Mark Shipped buttons on user account page

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,4 +1,8 @@
 class OrderItemsController < ApplicationController
   def update
+    @order_item = OrderItem.find_by(id: params[:id])
+
+    @order_item.update(id: params[:id], is_shipped: true)
+    redirect_to user_path(@order_item.product.user.id)
   end
 end
