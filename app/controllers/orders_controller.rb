92618fa106d59bc1c67808a822a659834fdc8da7
Winifred Irarrazaval <winirarrazaval@gmail.com>
2018-04-24 09:23:44 -0700

    added more features to the order controller, creating and order to check the products in it with a pending status and then update the order to add the payment method and user info

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,5 @@
 class OrdersController < ApplicationController
+  before_action :find_order, except: [:root, :index, :new, :create]
 
   def new
     @order = Order.new
@@ -13,8 +14,7 @@ class OrdersController < ApplicationController
       flash[:status] = :success
       flash[:result_text] = "Your order has been placed !"
       flash[:order_number] = "Order number: #{@order.id}"
-      session[:cart_id] = nil
-      redirect_to order_path(@order)
+      redirect_to edit_order_path(@order)
     else
 
       flash.now[:status] = :failure
@@ -25,14 +25,38 @@ class OrdersController < ApplicationController
     end
   end
 
-  def show
-    @order = Order.find_by(id: params[:id])
-    head :not_found unless @order
+  def show; end
+
+  def update
+    @order.update_attributes(order_params)
+
+    if @order.save
+
+      flash[:status] = :success
+      flash[:result_text] = "Your order has been placed !"
+      flash[:order_number] = "Order number: #{@order.id}"
+      session[:cart_id] = nil
+      redirect_to order_path(@order)
+    else
+
+      flash.now[:status] = :failure
+      flash.now[:result_text] = "Could not place your order"
+      flash.now[:messages] = @order.errors.messages
+
+      render :edit, status: :bad_request
+    end
+
   end
 
   private
   def order_params
     params.require(:order).permit(:name, :email, :creditcard, :name_on_card, :expiration_month, :expiration_year, :cvv, :mail_address, :billing_address, :zipcode, :status)
+  end
+
+  def find_order
+    @order = Order.find_by(id: params[:id])
+    head :not_found unless @order
+
   end
 
 end
