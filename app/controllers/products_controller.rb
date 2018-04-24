b0edef9e3707f2965d8878f7ac87e2987b583959
Wenjie <traxiaojie@gmail.com>
2018-04-24 10:44:31 -0700

    Created new, edit and update action in products controller Created pricate helper method and controller filter

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,17 +1,50 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only: [:show, :edit, :update, :destroy]
+
   # TODO: BUILD OUT HOMEPAGE VIEW FOR WHOLE SITE
   def homepage;end
 
   def index
-    @products = Product.all.paginate(:page => params[:page], :per_page => 5)
+    if params[:merchant_id]
+      @merchant = Merchant.find(params[:merchant_id])
+      @products = @merchant.products
+    else
+      @products = Product.all.paginate(:page => params[:page], :per_page => 5)
+    end
   end
   # TODO: ADD AS MANY CATEGORY FILTERS AS WINI DEVELOPS
 
-  def show
-    @product = Product.find_by(id: params[:id])
-    head :not_found unless @product
+  def new
+    @product = Product.new(merchant_id: params[:merchant_id])
+  end
 
+  def create
+    @product = Product.new()
+  end
+
+  def show
     @cartitem = Cartitem.new
     @review = Review.new
+  end
+
+  def edit; end
+
+  def update
+    @product.assign_attributes(product_params)
+    if @product.save
+      redirect_to product_path(@product)
+    else
+      render :edit, status: :bad_request
+    end
+  end
+
+  private
+  def product_params
+    return params.require(:product).permit(:name, :price, :description, :imgae, :stock, :visible, category_ids: [])
+  end
+
+  def find_product
+    @product = Product.find_by(id: params[:id])
+    head :not_found unless @product
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,15 @@ Rails.application.routes.draw do
 
 
   resources :products, only: [:index, :show]
-  resources :merchants
+
+  resources :merchants do
+    resources :products, only: [:index, :new]
+  end
+
+  resources :merchants do
+    resources :orders, only: [:index, :show]
+  end
+
   resources :orders
   resources :categories, only: [:index, :show]
   resources :carts
