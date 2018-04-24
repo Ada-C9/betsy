b8fc762b820dc5fa220930889076ac9c29d4c033
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-24 07:55:31 -0700

    progress toward getting the 'place order' button to work in the Cart controller.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -34,7 +34,21 @@ class CartController < ApplicationController
     redirect_to cart_path
   end
 
-  def update
+  def update_to_paid
+    @cart = Order.find_by(id: session[:cart_order_id])
+    @cart.status = "paid"
+    if !@cart.save
+      flash[:status] = :failure
+      flash[:result_text] = "We weren't able to process your order. Please double-check the form."
+      flash[:mssages] = @cart.errors.messages
+      redirect_to cart_path
+    else
+      flash[:status] = :success
+      flash[:result_text] = "Your order has been submitted!"
+      @order = Order.find_by(id: session[:cart_order_id])
+      session[:cart_order_id] = nil
+      render "orders/confirmation"
+    end
   end
 
   def destroy
@@ -70,71 +84,71 @@ class CartController < ApplicationController
 
   def total_quantity_requested
     @product = Product.find(params[:id])
-    end_quantity = 1
+    total_quantity = 1
     @cart.order_items.each do |order_item|
       if order_item.product_id == @product.id
-        end_quantity += order_item.quantity
+        total_quantity += order_item.quantity
       end
     end
-    return end_quantity
+    return total_quantity
   end
 end
 
-
-def add_to_cart
-  if session[:cart_order_id].nil? #If there is no cart yet
-    @order = Order.create status: "pending"
-    session[:cart_order_id] = @order.id
-    if @order.save
-      flash[:status] = :success
-      flash[:result_text] = "Welcome to the Puppsy shopping experience!"
-    else
-      flash[:status] = :failure
-      raise
-      flash[:result_text] = "We weren't able to create your shopping cart."
-      flash[:mssages] = @order.errors.messages
-      #some redirect, status: :bad_request
-    end
-  elsif @order = Order.find(session[:cart_order_id])
-    flash[:status] = :success
-  else
-    flash[:status] = :failure
-    flash[:result_text] = "We couldn't find your shopping cart."
-    flash[:mssages] = @order.errors.messages
-    #some redirect, status: :bad_request
-  end
-  if @product = Product.find(params[:id])
-    flash[:status] = :success
-    flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
-  else
-    flash[:status] = :failure
-    flash[:result_text] = "You have chosen an unrecognized product."
-    flash[:messages] = @product.errors.messages
-    render :index, status: :bad_request
-  end
-  duplicate = false
-  @order.order_items.each do |order_item|
-    if order_item.product.id == @product.id
-      order_item.quantity += 1
-      order_item.sav
-      duplicate = true
-      flash[:status] = :success
-      flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
-    end
-  end
-  unless duplicate == true
-    @order_item = OrderItem.create product_id: @product.id, order_id: @order.id, quantity: 1, is_shipped: "false"
-    if !@order_item.save
-      flash[:status] = :failure
-      flash[:result_text] = "Could not add this product to your cart."
-      flash[:messages] = @product.errors.messages
-    else
-      flash[:status] = :success
-      flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
-    end
-  end
-  redirect_to cart_path
-end
+#
+# def add_to_cart
+#   if session[:cart_order_id].nil? #If there is no cart yet
+#     @order = Order.create status: "pending"
+#     session[:cart_order_id] = @order.id
+#     if @order.save
+#       flash[:status] = :success
+#       flash[:result_text] = "Welcome to the Puppsy shopping experience!"
+#     else
+#       flash[:status] = :failure
+#       raise
+#       flash[:result_text] = "We weren't able to create your shopping cart."
+#       flash[:mssages] = @order.errors.messages
+#       #some redirect, status: :bad_request
+#     end
+#   elsif @order = Order.find(session[:cart_order_id])
+#     flash[:status] = :success
+#   else
+#     flash[:status] = :failure
+#     flash[:result_text] = "We couldn't find your shopping cart."
+#     flash[:mssages] = @order.errors.messages
+#     #some redirect, status: :bad_request
+#   end
+#   if @product = Product.find(params[:id])
+#     flash[:status] = :success
+#     flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
+#   else
+#     flash[:status] = :failure
+#     flash[:result_text] = "You have chosen an unrecognized product."
+#     flash[:messages] = @product.errors.messages
+#     render :index, status: :bad_request
+#   end
+#   duplicate = false
+#   @order.order_items.each do |order_item|
+#     if order_item.product.id == @product.id
+#       order_item.quantity += 1
+#       order_item.sav
+#       duplicate = true
+#       flash[:status] = :success
+#       flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
+#     end
+#   end
+#   unless duplicate == true
+#     @order_item = OrderItem.create product_id: @product.id, order_id: @order.id, quantity: 1, is_shipped: "false"
+#     if !@order_item.save
+#       flash[:status] = :failure
+#       flash[:result_text] = "Could not add this product to your cart."
+#       flash[:messages] = @product.errors.messages
+#     else
+#       flash[:status] = :success
+#       flash[:result_text] = "#{@product.name} has been successfully added to your cart!"
+#     end
+#   end
+#   redirect_to cart_path
+# end
 
 
 # def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,30 @@
 Rails.application.routes.draw do
 
   root 'homepage#index'
+
   get '/homepage', to:'homepage#index', as:'homepage'
+
   get '/auth/:provider/callback', as: 'auth_callback', to: 'sessions#create'
 
   get '/auth/github', as: 'github_login'
 
-  # get '/cart', to: 'order#', as: "empty_cart"
-
-  # Access_cart only worked as a GET method.
-
+  delete "/logout", to: "sessions#destroy", as: "logout"
 
   get '/cart', to: 'cart#access_cart', as: "cart"
 
   patch '/cart/:id', to: 'cart#update'
 
+  patch '/cart', to: 'cart#update_to_paid', as: "place_order"
+
   delete '/cart/:id/remove_single_item', to:'cart#remove_single_item', as: "remove_single_item"
 
   delete '/cart/delete', to:'cart#destroy', as: "cart_destroy"
 
-
-
-  delete "/logout", to: "sessions#destroy", as: "logout"
-
   post '/products/:id/add_to_cart', to: 'cart#add_to_cart', as: 'add_to_cart'
 
   get 'orders/:id/confirmation', to: 'orders#confirmation', as: 'order_confirmation'
-  put 'orders/:id/cancel', to: 'orders#cancel', as: 'order_cancel'
 
+  put 'orders/:id/cancel', to: 'orders#cancel', as: 'order_cancel'
 
   resources :orders
   resources :sessions, except: [:destroy]
