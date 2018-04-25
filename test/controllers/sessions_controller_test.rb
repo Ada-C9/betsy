27b3c58564797dfa8465d1481cfdca77bce7d91e
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-25 13:43:15 -0700

    Revision of first Oauth login test.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require 'pry'
 
 describe SessionsController do
 
@@ -17,12 +18,14 @@ describe SessionsController do
 
       get auth_callback_path(:github)
 
+
+      flash[:result_text].must_equal "Logged in successfully"
       must_redirect_to root_path
       session[:user_id].must_equal user.id
       User.count.must_equal start_count
 
     end
-
+    
     it "creates an account for a new user and redirects to the root route" do
 
       start_count = User.count
@@ -33,29 +36,38 @@ describe SessionsController do
 
       get auth_callback_path(:github)
 
-      must_redirect_to root_path
-      session[:user_id].must_equal User.last.id
+      must_respond_with :success
+      user.username.must_equal "drywall_bob"
       User.count.must_equal start_count + 1
-
-    end
-
-    it "redirects to the login route if given invalid user data" do
-
-      start_count = User.count
-
-      user = User.new(provider: "github", uid: 40420, username: "", email: "!!!!!!!!")
-
-      OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
-
-      get auth_callback_path(:github)
-
       must_redirect_to root_path
 
-      session[:user_id].must_be_nil
+      session_id = session[:user_id]
 
-      User.count.must_equal start_count
+      binding.pry
+
+      user.id.must_equal User.last.id
+
+      session[:user_id].must_equal user.id
+
 
     end
+    #
+    # it "redirects to the login route if given invalid user data" do
+    #
+    #   start_count = User.count
+    #
+    #   user = User.new(provider: "github", uid: 40420, username: "", email: "!!!!!!!!")
+    #
+    #   OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new(mock_auth_hash(user))
+    #
+    #   get auth_callback_path(:github)
+    #
+    #   flash[:status] = :failure
+    #   must_redirect_to root_path
+    #   session[:user_id].must_be_nil
+    #   User.count.must_equal start_count
+    #
+    # end
   end
 
   describe "index" do
@@ -65,3 +77,98 @@ describe SessionsController do
   end
 
 end
+
+# describe SessionsController do
+#
+#   describe "login_form" do
+#
+#     it "must succeed" do
+#
+#       get login_path
+#       must_respond_with :success
+#
+#     end
+#
+#   end
+#
+#   describe "login" do
+#
+#     before do
+#
+#       @unknown_user_name = "Yolanda"
+#
+#     end
+#
+#     it "must succeed for an existing user" do
+#
+#       user_d = users(:dan)
+#
+#       post login_path, params: {username: "dan"}
+#
+#       session[:user_id].must_equal user_d.id
+#
+#       must_redirect_to root_path
+#
+#
+#     end
+#
+#     it "must succeed for a new user with a valid name" do
+#
+#       existing_user = User.find_by(username: @unknown_user_name)
+#
+#       existing_user.must_be_nil
+#
+#       post login_path, params: {username: @unknown_user_name}
+#
+#       session[:user_id].wont_be_nil
+#       must_redirect_to root_path
+#
+#     end
+#
+#     it "must create a new user instance when it recieves a new, valid username" do
+#
+#       #Just proves that the test works
+#       existing_user = User.find_by(username: @unknown_user_name)
+#       existing_user.must_be_nil
+#
+#
+#       before_user_count = User.all.count
+#
+#       post login_path, params: {username: @unknown_user_name}
+#
+#       after_user_count = User.all.count
+#
+#       (after_user_count - before_user_count).must_equal 1
+#       new_user = User.find_by(username: @unknown_user_name)
+#       new_user.username.must_equal @unknown_user_name
+#       session[:user_id].must_equal new_user.id
+#
+#     end
+#
+#     it "must fail for a new user who doesn't enter a user name" do
+#
+#       null_username = ""
+#
+#       post login_path, params: {username: null_username}
+#
+#       post login_path must_respond_with :bad_request
+#
+#     end
+#
+#   end
+#
+#   describe "logout" do
+#
+#     it "must succeed" do
+#
+#       post login_path, params: {username: "testy test"}
+#       session[:user_id].wont_be_nil
+#
+#       post logout_path
+#
+#       session[:user_id].must_be_nil
+#
+#     end
+#   end
+#
+# end
