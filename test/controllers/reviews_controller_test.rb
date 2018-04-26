753478e64e4626f416d76d10ef0cbe2dce754a11
Monalisa Chatterjee <usermonalisa@gmail.com>
2018-04-26 00:53:24 -0700

    Removed binding.pry

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -9,33 +9,24 @@ describe ReviewsController do
       proc{
         post product_reviews_path(products(:product_4).id), params:{
           review: {rating:reviews(:review_4).rating,
-                  content:reviews(:review_4).content,
-                  product:reviews(:review_4).product }
-      }
-    }.must_change 'Review.count', 1
-      must_respond_with :redirect
-    end
-
-    it 'will render a bad request for an invalid review' do
-      binding.pry
-      proc{
-        post product_reviews_path(products(:product_4).id), params:{
-          review: {rating:"",
-                  content:reviews(:review_4).content,
-                  product:reviews(:review_4).product }
+            content:reviews(:review_4).content,
+            product:reviews(:review_4).product }
           }
-    }.must_change 'Review.count', 0
-    binding.pry
-      must_respond_with :bad_request
+        }.must_change 'Review.count', 1
+        must_respond_with :redirect
+      end
+
+      it 'will render a bad request for an invalid review' do
+        proc{
+          post product_reviews_path(products(:product_4).id), params:{
+            review: {rating:"",
+              content:reviews(:review_4).content,
+              product:reviews(:review_4).product }
+            }
+          }.must_change 'Review.count', 0
+          must_respond_with :bad_request
+        end
+      end
+
+      #end of class
     end
-
-
-
-
-
-  end
-
-
-
-  #end of class
-end
