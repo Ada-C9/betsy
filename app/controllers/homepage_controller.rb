7f7f4ef7fb14eb66b7d8d966e31dc43401967e70
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-04-27 06:07:50 -0700

    fixed a typo on the homepage.

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,9 +1,9 @@
 class HomepageController < ApplicationController
   def index
     @popular_products = Product.top_sellers
-    category_outfit = Category.find_by(name: "outfit")
+    category_outfit = Category.find_by(name: "Outfit")
     @outfits = category_outfit.products
-    category_food = Category.find_by(name: "food")
+    category_food = Category.find_by(name: "Food")
     @foods = category_food.products
     @categories = Category.all
 
