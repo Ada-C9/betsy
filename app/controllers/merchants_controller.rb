01b46bc926acd178bbb9da2f37f1bf784e04ff0e
Wenjie <traxiaojie@gmail.com>
2018-04-27 13:07:38 -0700

    Fixed products display

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,28 +1,26 @@
 class MerchantsController < ApplicationController
-before_action :find_merchant, only: [:show, :display, :show_products ]
-
-
   def index
     @merchants = Merchant.all.paginate(:page => params[:page], :per_page => 3)
-
   end
 
   def show
+    @merchant = Merchant.find_by(id: session[:merchant_id])
     if session[:merchant_id].to_s == params[:id]
       @orders = @merchant.my_orders
     else
-
-        flash[:status] = :failure
-        flash[:result_text] = "You can not view other merchant's account page"
-        redirect_back fallback_location: root_path
-
+      flash[:status] = :failure
+      flash[:result_text] = "You can not view other merchant's account page"
+      redirect_back fallback_location: root_path
     end
   end
 
-  def display; end
+  def display
+    @merchant = Merchant.find_by(id: params[:merchant_id])
+  end
 
   def show_products
     @products = Product.where(merchant_id: params[:id])
+    @merchant = Merchant.find_by(id: params[:id])
   end
 
   def by_name
@@ -30,13 +28,6 @@ before_action :find_merchant, only: [:show, :display, :show_products ]
     @merchants = Merchant.where("LOWER(username) like ?", "%#{merchant_name.downcase}%")
 
     render :index
-  end
-
-  private
-
-  def find_merchant
-  @merchant = Merchant.find_by(id: params[:id])
-    head :not_found unless @merchant
   end
 
 end
